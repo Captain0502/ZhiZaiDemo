0a1768a9362d57b9548617c725b9d17c3c65396e
Captain <wangzhangchuan0502@163.com>
2017-10-19 13:24:23 +0800

    Initial Commit

---
+++ ZhiZaiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZhiZaiDemo
+//
+//  Created by wangzhangchuan on 2017/10/19.
+//  Copyright © 2017年 wangzhangchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZhiZaiDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhiZaiDemo
+//
+//  Created by wangzhangchuan on 2017/10/19.
+//  Copyright © 2017年 wangzhangchuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
